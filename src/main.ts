812972e8e1b1514d84ad10a63b358e22ba08b23c
DittGitHubNamn <DinEmail@exempel.com>
2025-11-18 23:05:57 +0100

    my webb, in progrecc

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "./style.css";
+const ulList = document.getElementById("ulList");
+const pictures = document.querySelector(".pictures");
+const cards = document.querySelectorAll(".card");
+const menyContainer = document.querySelector(".menyContainer");
+
+const turnCard = () => {
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      if (card.style.backgroundColor === "green") {
+        card.style.backgroundColor = ""; // återgår till ursprungsfärg
+      } else {
+        card.style.backgroundColor = "green";
+      }
+    });
+  });
+};
+turnCard();
