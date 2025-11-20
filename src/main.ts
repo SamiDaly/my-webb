c055d7dde5d676034bf0fc4d6c282e94784ca848
DittGitHubNamn <DinEmail@exempel.com>
2025-11-20 12:21:39 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ const cards = document.querySelectorAll(".card");
 const menyContainer = document.querySelector(".menyContainer");
 
 const turnCard = () => {
+  const link; //??????????????
   cards.forEach((card) => {
+    li.textContent = item;
+    const li = document.createElement("li"); // tillägg ??
+    ulList.appendChild(li);
     card.addEventListener("click", () => {
       if (card.style.backgroundColor === "green") {
         card.style.backgroundColor = ""; // återgår till ursprungsfärg
